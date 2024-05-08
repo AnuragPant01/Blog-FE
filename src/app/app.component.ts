0ec3139a6ee67046a96c1df07b52a4bed18030de
anurag <anurag.pant@jungleworks.com>
2024-05-08 12:53:28 +0530

    Feat: Blogs Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blogs';
+  blogForm!:FormGroup;
+  isSubmit:boolean = false;
+  blogsData:any = []
+
+  constructor(public fb:FormBuilder,private http:HttpClient){}
+
+  ngOnInit(){
+
+    this.blogForm = this.fb.group({
+      'title' : ['',Validators.required],
+      'description' : ['',Validators.required]
+    })
+
+    this.fetchData()
+
+  }
+
+  fetchData(){
+    this.http.get('http://localhost:3030/blogs').subscribe((res:any)=>{
+      if(res){
+        this.blogsData =res
+      }
+    },err=>{
+      console.log('error while getting data')
+    })
+  }
+
+  handleSubmit(){
+    this.isSubmit = true;
+    if(!this.blogForm.valid){
+      return
+    }
+    const data = this.blogForm.value
+    this.http.post('http://localhost:3030/blogs',data).subscribe((res:any)=>{
+      console.log('data send');
+      this.isSubmit = false
+      this.blogForm.reset();
+      this.fetchData()
+    },err=>{
+      console.log('error while sending data')
+    })
+  }
 }
